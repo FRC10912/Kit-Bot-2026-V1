84a911733a1845a8823b17cf3b02e5af1826ba96
Winders Arias <Windersari@gmail.com>
2026-01-24 13:51:24 -0500

    Disabled Fuel Subsystem Actions

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -6,12 +6,12 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.CANFuelSubsystem;
+// import frc.robot.subsystems.CANFuelSubsystem;
 import frc.robot.subsystems.CANDriveSubsystem;
 
 public final class Autos {
   // Example autonomous command which drives forward for 1 second.
-  public static final Command exampleAuto(CANDriveSubsystem driveSubsystem, CANFuelSubsystem ballSubsystem) {
+  public static final Command exampleAuto(CANDriveSubsystem driveSubsystem) {//, CANFuelSubsystem ballSubsystem) {
     return new SequentialCommandGroup(
         // Drive backwards for .25 seconds. The driveArcadeAuto command factory
         // creates a command which does not end which allows us to control
@@ -19,12 +19,14 @@ public final class Autos {
         driveSubsystem.driveArcade(() -> 0.5, () -> 0).withTimeout(.25),
         // Stop driving. This line uses the regular driveArcade command factory so it
         // ends immediately after commanding the motors to stop
-        driveSubsystem.driveArcade(() -> 0, () -> 0),
-        // Spin up the launcher for 1 second and then launch balls for 9 seconds, for a
-        // total of 10 seconds
-        ballSubsystem.spinUpCommand().withTimeout(1),
-        ballSubsystem.launchCommand().withTimeout(9),
-        // Stop running the launcher
-        ballSubsystem.runOnce(() -> ballSubsystem.stop()));
+        driveSubsystem.driveArcade(() -> 0, () -> 0));
+
+        // --- Not in use --- 
+        // // Spin up the launcher for 1 second and then launch balls for 9 seconds, for a
+        // // total of 10 seconds
+        // ballSubsystem.spinUpCommand().withTimeout(1),
+        // ballSubsystem.launchCommand().withTimeout(9),
+        // // Stop running the launcher
+        // ballSubsystem.runOnce(() -> ballSubsystem.stop()));
   }
 }
